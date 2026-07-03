9b96d63b749d3415568e2e97a2c63f5b2bb89b8c
Ali <ali.al.jobouri.97@gmail.com>
2026-07-03 19:41:31 +0300

    adding google auth

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 import express from "express";
 import bodyParser from "body-parser";
-import axios from "axios";
 import pg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
+env.config();
 
-const db = new pg.Client({
+app.use(
+    session({
+        secret: "TOPSECRET",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*const db = new pg.Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
+});*/
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "books",
+    password: "135264",
+    port: 5433,
 });
 
 db.connect();
@@ -58,16 +83,42 @@ async function getBooks(sort = "id") {
 //---------------------------------------------------------------------------------------------------
 
 // Routes
+app.get("/index", async (req, res) => {
+    if (req.isAuthenticated()) {
+        const sort = req.query.sort || "recent";
+
+        const books = await getBooks(sort);
+        console.log("User:", req.user);
+        res.render("index.ejs", {
+            currentPage: "books",
+            books,
+            sort,
+            username: req.user.name
+        });
+    } else {
+        res.redirect("/auth");
+    }
+});
+
 app.get("/", async (req, res) => {
-    const sort = req.query.sort || "recent";
+    if (req.isAuthenticated()) {
+        const sort = req.query.sort || "recent";
 
-    const books = await getBooks(sort);
+        const books = await getBooks(sort);
+        console.log("User:", req.user);
+        res.render("index.ejs", {
+            currentPage: "books",
+            books,
+            sort,
+            username: req.user.name
+        });
+    } else {
+        res.redirect("/auth");
+    }
+});
 
-    res.render("index.ejs", {
-        currentPage: "books",
-        books,
-        sort
-    });
+app.get("/auth", async (req, res) => {
+    res.render("auth");
 });
 
 app.get("/new", (req, res) => {
@@ -150,7 +201,16 @@ app.get("/comments/:id", async (req, res) => {
     const bookId = req.params.id;
 
     const commentsResult = await db.query(
-        "SELECT * FROM comments WHERE book_id = $1 ORDER BY id",
+        `SELECT
+            comments.id AS id,
+            comments.book_id,
+            comments.comment,
+            users.name AS username
+        FROM comments
+        JOIN users
+            ON comments.user_id = users.id
+        WHERE comments.book_id = $1
+        ORDER BY comments.id;`,
         [bookId]
     );
 
@@ -184,8 +244,8 @@ app.post("/books/:id/comments", async (req, res) => {
     const content = req.body.content;
 
     await db.query(
-        "INSERT INTO comments (book_id, comment) VALUES ($1, $2)",
-        [bookId, content]
+        "INSERT INTO comments (book_id, comment, user_id) VALUES ($1, $2, $3)",
+        [bookId, content, req.user.id]
     );
 
     res.redirect(`/comments/${bookId}`);
@@ -217,6 +277,79 @@ app.post("/delete/:id", async (req, res) => {
     );
 
     res.redirect("/");
+});
+
+//----------------------AUTHENTICATION--------------------\\
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    prompt: "select_account",
+  })
+);
+
+app.get(
+    "/auth/google/index",
+    passport.authenticate("google", {
+        successRedirect: "/index",
+        failureRedirect: "/auth",
+    })
+);
+app.post("/auth",
+    passport.authenticate("google", {
+        successRedirect: "/index",
+        failureRedirect: "/auth",
+    })
+);
+
+app.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+passport.use(
+    "google",
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/auth/google/index",
+            userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+        },
+        async (accessToken, refreshToken, profile, cb) => {
+            try {
+                console.log(profile);
+                const result = await db.query("SELECT * FROM users WHERE email = $1", [
+                    profile.email,
+                ]);
+                if (result.rows.length === 0) {
+                    const newUser = await db.query(
+                        "INSERT INTO users (email, password, name) VALUES ($1, $2, $3) RETURNING *",
+                        [profile.email, "google", profile.displayName]
+                    );
+                    return cb(null, newUser.rows[0]);
+                } else {
+                    console.log("Here");
+                    console.log(result.rows[0]);
+                    return cb(null, result.rows[0]);
+                }
+            } catch (err) {
+                return cb(err);
+            }
+        }
+    )
+);
+
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+    cb(null, user);
 });
 
 app.listen(port, () => {
